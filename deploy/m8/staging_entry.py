f48852b639e0a56236f5b25d2ca6d12ae40a62d8
mark-owa <markjoshuagalit2@gmail.com>
2026-09-18 10:31:37 +0800

    Allow staging UI module sources at root only

--- deploy/m8/staging_entry.py
+++ deploy/m8/staging_entry.py
@@ -1,5 +1,24 @@
+from fastapi import Request
 from fastapi.responses import FileResponse
+
 from app.main import app
+
+
+@app.middleware("http")
+async def staging_frontend_csp(request: Request, call_next):
+    response = await call_next(request)
+    if request.url.path == "/":
+        response.headers["Content-Security-Policy"] = (
+            "default-src 'self'; "
+            "script-src 'self' 'unsafe-inline' blob: https://esm.sh; "
+            "style-src 'self' 'unsafe-inline'; "
+            "connect-src 'self' https://esm.sh; "
+            "img-src 'self' data: blob:; "
+            "font-src 'self' data:; "
+            "object-src 'none'; base-uri 'self'; frame-ancestors 'none'"
+        )
+    return response
+
 
 @app.get("/", include_in_schema=False)
 def staging_frontend():
